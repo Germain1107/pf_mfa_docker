0d150c007566ccf7e269b0e6bd1fe6f1b78b2d3d
German <germainstephens07@gmail.com>
2022-12-10 15:52:31 -0500

    direccion compatible con linux systems

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os.path import abspath
 
 app = Flask(__name__)
-db_path = abspath('.') + "\\databases\\users.db"
+db_path = abspath('.') + "/databases/users.db"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "secre1234"
